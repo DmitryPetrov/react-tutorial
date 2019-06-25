7bad57672f90052343018075e221e6be66f53d30
Dmitry Petrov <dmitrypetrov93gmail.com>
2019-06-25 16:49:17 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import FilterableProductTable from './FilterableProductTable';
+import * as serviceWorker from './serviceWorker';
+import PRODUCTS from './products'
+
+
+ReactDOM.render(
+  <FilterableProductTable products={PRODUCTS}/>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
